57741f194789dff5046739c81a64bafd97fb2d20
Mauro Favre <mauro_favre@hotmail.com>
2024-09-08 13:25:32 -0300

    Agregar archivos de proyecto.

---
+++ gestionInventario1/ClsProductos1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//Agregados 
+using System.Data;
+using System.Data.OleDb;
+using System.Windows.Forms; 
+   
+
+namespace gestionInventario1
+{
+    internal class ClsProductos1
+    {
+        //creamos objeto para conectarnos con la bd
+        private OleDbConnection conexion = new OleDbConnection();
+        //para enviar las ordenes a la bd 
+        private OleDbCommand comando = new OleDbCommand();
+        //nos sirve para adaptar los datos que estan mal en la bd  
+        private OleDbDataAdapter adaptador = new OleDbDataAdapter();
+
+        private string cadenaConexion = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source = baseDatos\\inventario.accdb";
+        private string Tabla = "Productos";
+
+
+        public void Listar(DataGridView Grilla)
+        {
+            //recibe la cadena de conexion
+            conexion.ConnectionString = cadenaConexion;
+            conexion.Open();
+
+            //comandos de ordenes
+            comando.Connection = conexion;
+            //ponemos el tipo de comando, (3 tipos
+            //text(envia instrucciones sql),
+            //tabletDirect(trae la tabla))
+            comando.CommandType = CommandType.TableDirect;
+            //nombre de la tabla que vamos a traer 
+            comando.CommandText = Tabla; 
+
+
+
+            //adaptamos el comando configurado
+            adaptador = new OleDbDataAdapter(comando);
+            //objeto de clase dataset para poder cargar los datos 
+            DataSet ds = new DataSet();
+            //adaptamos el dataset
+            adaptador.Fill(ds);
+
+            //dATAsOURCE TOMA EL CONTENDIDO COMPLETO DEL DATASET
+            Grilla.DataSource = ds.Tables[0];
+
+            //cerramos la conexion 
+            conexion.Close();
+
+        }
+
+    }
+}
---
+++ gestionInventario1/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace gestionInventario1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPrueba = new System.Windows.Forms.Button();
+            this.Grilla = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Grilla)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPrueba
+            // 
+            this.btnPrueba.Location = new System.Drawing.Point(45, 384);
+            this.btnPrueba.Name = "btnPrueba";
+            this.btnPrueba.Size = new System.Drawing.Size(75, 23);
+            this.btnPrueba.TabIndex = 0;
+            this.btnPrueba.Text = "Listar Todo";
+            this.btnPrueba.UseVisualStyleBackColor = true;
+            this.btnPrueba.Click += new System.EventHandler(this.btnPrueba_Click);
+            // 
+            // Grilla
+            // 
+            this.Grilla.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Grilla.Location = new System.Drawing.Point(343, 100);
+            this.Grilla.Name = "Grilla";
+            this.Grilla.Size = new System.Drawing.Size(352, 270);
+            this.Grilla.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(59, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(147, 72);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Conexion";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(59, 171);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(147, 66);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Conexion";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(59, 266);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(147, 74);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Conexion";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Grilla);
+            this.Controls.Add(this.btnPrueba);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.Grilla)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPrueba;
+        private System.Windows.Forms.DataGridView Grilla;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
